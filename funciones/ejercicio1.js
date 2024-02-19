dff0f4822ef72d924673d21932910f90e5d186aa
unknown <itrevixx@gmail.com>
2024-02-19 18:07:06 +0100

    a

---
+++ funciones/ejercicio1.js
@@ -0,0 +1,6 @@
+/* Crear y llamar a una función que reciba como parámetro de entrada un valor booleano y muestre «Sí, eso es cierto» si el valor es true, y «No, eso es falso» si el valor es false. */
+
+function boolean(bool) {
+    console.log(bool ? "Sí, eso es cierto" : "No, eso es falso");
+}
+boolean(false);
